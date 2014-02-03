88f992d4e4664006d3b05b6fe39cf3829a6662eb
Liobouchan <leo.bouchan@gmail.com>
2014-02-03 15:51:34 -0600

    Code arregled in order

--- 1-2.c
+++ 1-2.c
@@ -1,27 +1,58 @@
-/* Programa al que se le pasa una cadena de caracteres en minusculas */
-/* y llama a una funcion que convertira esa cadena en mayusculas */
 #include <stdio.h>
 #include <conio.h>
-#include <stdlib.h> 
 
-void MinToMay(char string[]); /* cabecera de la funcion */
+void ingreso(char *);
+void mayusculas(char *);
+void minusculas(char *);
 
-/* Funcion que convierte minusculas a mayusculas */
-void MinToMay(char string[]) { 
-  int i=0; 
-  int desp='a'-'A'; 
-  for (i=0;string[i]!='\0';++i) { 
-    if(string[i]>='a'&&string[i]<='z') { 
-      string[i]=string[i]-desp; 
-    } 
-  } 
+int main(void){
+
+  char string[256];
+  ingreso(string);
+
+  printf("FRASE ORIGINAL: %s\n", string);
+
+  mayusculas(&string[0]);
+
+  printf("MAYUSCULA: %s\n",string);
+
+  minusculas(string);
+
+  printf("MINUSCULA: %s\n",string);
+
+  getch();
+
 }
 
-int main() {
-  char cadena[80];
+void ingreso(char *s){
+
   system ("cls");
-  printf("Introduce una cadena:");
-  gets(cadena);
-  MinToMay(cadena); /* Llama a la funcion */
-  printf ("En Mayusculas: %s\n", cadena); getch(); 
+
+  printf("INGRESE UNA FRASE\n");
+
+  gets(s);
+
+}
+
+void mayusculas(char *s){
+
+for( ; *s!='\0'; s++)
+  if(*s>='a' && *s<='z')
+    *s-=(char)32;
+
+}
+
+void minusculas(char *s){
+
+while(*s!='\0'){
+
+if(*s>='A' && *s<='Z')
+  *s+=(char)32;
+  s++;
+}
+
+}
+
+void convierte(char *s){
+    
 }
