f8ba1461e7ff985b823bbdf23bc280bec01fbb80
Liobouchan <leo.bouchan@gmail.com>
2014-02-03 16:36:27 -0600

    Eercise 1 part 2 finished

--- 1-2.c
+++ 1-2.c
@@ -1,55 +1,35 @@
 #include <stdio.h>
 #include <conio.h>
 
-void ingreso(char *);
-void mayusculas(char *);
-void minusculas(char *);
+void texto(char *);
+void convertir(char *);
 
-void ingreso(char *s){
+void texto(char *s){
   system ("cls");
   printf("Ingresar una cadena: \n");
   gets(s);
 }
 
-void mayusculas(char *s){
-
-for( ; *s!='\0'; s++){
-  if(*s>='a' && *s<='z'){
-    *s-=(char)32;
-}else{
-    while(*s!='\0'){
- *s+=(char)32;
-  s++;
+void convertir(char *s){
+  for( ; *s!='\0'; s++){
+    if(*s>='a' && *s<='z'){
+      *s-=(char)32;
+    }else{
+      while(*s!='\0'){
+        *s+=(char)32;
+        s++;
+      }
+    }
+  }
 }
-}
-}
-}
-
 
 int main(void){
-
   char string[256];
 
-  ingreso(string);
+  texto(string);
 
-  mayusculas(&string[0]);
-  printf("Convertido a mayusculas: %s\n",string);
-/*
-  minusculas(string);
-  printf("Convertido a minusculas: %s\n",string);
-*/
+  convertir(&string[0]);
+  printf("Tu texto convertido: %s\n",string);
+
   getch();
-
-}
-
-
-void minusculas(char *s){
-
-while(*s!='\0'){
-
-if(*s>='A' && *s<='Z')
-  *s+=(char)32;
-  s++;
-}
-
 }
