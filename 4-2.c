fcef3f544c6396659f253e2e4a2d59ee8ca77a07
Liobouchan <leo.bouchan@gmail.com>
2014-02-03 17:04:48 -0600

    4-2 Arregled and is in function

--- 4-2.c
+++ 4-2.c
@@ -1,1 +1,35 @@
-int validarPalindroma( char *cad );
+#include <stdio.h>
+#include <string.h>
+
+int validarPalindroma( char cad[] );
+
+int validarPalindroma( char cad[]){
+  int i,j;
+  int palindromo = 1;
+
+  j=strlen(cad)-1;
+    for(i=0; i<strlen(cad)/2; i++, j--) {
+      printf("Comprobando %c==%c\n", *(cad+i), *(cad+j));
+      if (*(cad+i)!=*(cad+j)) {
+        palindromo = 0;
+        break;
+      }
+    }
+    if (palindromo)
+      printf("\nEs un palíndrimo.\n");
+    else
+      printf("\nNo es un palíndrimo.\n");
+}
+
+int main()
+{
+    char cad[20];
+    int i, j;
+    int palindromo = 1;
+    printf("Escribe una cadena: ");
+    fflush(stdout);
+    gets(cad);
+    validarPalindroma(cad);
+ 
+    return (0);
+}
