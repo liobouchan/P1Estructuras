34114532540c1f5c42a2fda92465bbffa2d98002
Liobouchan <leo.bouchan@gmail.com>
2014-02-03 18:11:11 -0600

    7-2 Finished missing the comments

--- 7-2.c
+++ 7-2.c
@@ -1,1 +1,37 @@
-7-2.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+void copiarApuntador (char*,char*);
+void copiarString (char* , char*);
+
+int main(){
+  char *c1, *c2;
+ 
+  c1=(char*)malloc(20*sizeof(char));
+  c2=(char*)malloc(20*sizeof(char));
+   
+  if ((c1==NULL)||(c2==NULL)){
+    printf ("Error de menu");
+    exit (-1);
+  }
+
+  printf ("introduce una cadena: ");
+  gets (c1);
+
+  copiarApuntador (c1,c2);
+  printf("Con Operaciones de Punteros %s\n",c2);
+
+  copiarString (c1,c2);
+}
+ 
+void copiarApuntador (char *c1, char *c2){
+  while(*c1 != '\0'){
+    *c2++ = *c1++;
+  }
+}
+
+void copiarString (char *c1, char *c2){
+  strcpy (c2 , c1);
+  printf("Con operaciones String %s\n", c2 );
+}
