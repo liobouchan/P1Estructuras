7414595bdbf884de8e87e72049b9862deea5465b
Liobouchan <leo.bouchan@gmail.com>
2014-02-03 21:54:22 -0600

    4-2 Comentado

--- 4-2.c
+++ 4-2.c
@@ -13,17 +13,20 @@
 int validarPalindroma( char cad[] );
 
 int validarPalindroma( char cad[]){
-  int i,j;
-  int palindromo = 1;
+  int i,j; //Variables que usaremos para comparar
+  int palindromo = 1; //Variable declarada con 1 en caso de que si sea palindromo
 
   j=strlen(cad)-1;
+  //Compararemos letra por letra para ver que coincidan
   for(i=0; i<strlen(cad)/2; i++, j--) {
     printf("Comprobando %c==%c\n", *(cad+i), *(cad+j));
+    //Si las letras no coinciden asignamos a un 0 a palindromo
     if (*(cad+i)!=*(cad+j)) {
       palindromo = 0;
       break;
     }
   }
+  //Imprimimos el resultado
   if (palindromo)
     printf("\nEs un palindrimo.\n");
   else
@@ -31,11 +34,13 @@ int validarPalindroma( char cad[]){
 }
 
 int main(){
-  char cad[20];
+  char cad[20]; //Variable para guardar lo que el usuario ingresa
 
+  //Pedimos al usuario que ingrese la cadena y lo guardamos
   printf("Escribe una cadena: ");
   fflush(stdout);
   gets(cad);
+  //Mandamos la cadena guardada ala funcion que nos valida el palindromo
   validarPalindroma(cad);
 
   return (0);
